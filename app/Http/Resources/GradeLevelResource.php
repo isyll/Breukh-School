f3e73177e3abe6090914d7476ddd709f1d0db9c3
Ibrahima Sylla <syllaisa6@gmail.com>
2023-06-26 13:02:51 +0000

    create GradeLevelResource

---
+++ app/Http/Resources/GradeLevelResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GradeLevelResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name'       => $this->name,
+            'cycle_name' => $this->cycle_name,
+            'nb_cycles'  => $this->nb_cycles,
+            'classes'    => ClasseResource::collection($this->classes)
+        ];
+    }
+}
