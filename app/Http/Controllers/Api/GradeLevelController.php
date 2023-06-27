683d57e3aaf09112b6dcb93a60214b0e813efb15
Ibrahima Sylla <syllaisa6@gmail.com>
2023-06-27 14:57:26 +0000

    add show() method and join feature to all()

--- app/Http/Controllers/Api/GradeLevelController.php
+++ app/Http/Controllers/Api/GradeLevelController.php
@@ -5,11 +5,41 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\GradeLevelResource;
 use App\Models\GradeLevel;
+use App\Traits\JoinQueryParams;
+use Illuminate\Http\Request;
 
 class GradeLevelController extends Controller
 {
-    public function all()
+    use JoinQueryParams;
+
+    private array $relations = [
+        'classes'
+    ];
+
+    public function all(Request $request)
     {
-        return GradeLevelResource::collection(GradeLevel::all());
+        $collection = GradeLevel::all();
+
+        if ($join = $request->input('join')) {
+            $join_items = str_contains($join, ',')
+                ? collect(explode(',', $join))
+                : collect([$join]);
+
+            foreach ($join_items as $i) {
+                $collection->when(
+                    in_array($i, $this->relations),
+                    function ($collection) use ($i) {
+                        $collection = $collection->load($i);
+                    }
+                );
+            }
+        }
+
+        return $collection;
+    }
+
+    public function show(GradeLevel $grade_level)
+    {
+        return $grade_level;
     }
 }
