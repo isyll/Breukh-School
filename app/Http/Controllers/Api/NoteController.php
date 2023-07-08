e8e12dc2bba27ddfeb82a1db66b7d48d9ec9c801
Ibrahima Sylla <syllaisa6@gmail.com>
2023-07-08 21:18:25 +0000

    fix student & classe non concordance error

--- app/Http/Controllers/Api/NoteController.php
+++ app/Http/Controllers/Api/NoteController.php
@@ -11,6 +11,7 @@ use App\Models\Enrollment;
 use App\Models\Evaluation;
 use App\Models\Note;
 use App\Models\SchoolYear;
+use App\Models\Student;
 use App\Models\Subject;
 use Illuminate\Support\Facades\Validator;
 
@@ -34,6 +35,17 @@ class NoteController extends Controller
             ->where('school_year_id', $year)
             ->where('subject_id', $subject->id)->first();
 
+        if (!$classeSubject) {
+            return response()->json([
+                'errors' => [
+                    __(
+                        'classe_does_not_have_this_subject',
+                        ['classe' => $classe->name, 'subject' => $subject->name]
+                    )
+                ]
+            ], 422);
+        }
+
         foreach ($datas as $data) {
             $validator = Validator::make($data, [
                 'student' => 'required|exists:students,id',
@@ -56,50 +68,52 @@ class NoteController extends Controller
                     ->where('school_year_id', $year)
                     ->first();
 
-                if ($classeSubject) {
-                    $note = Note::where('enrollment_id', $enrollment->id)
-                        ->where('classe_subject_id', $classeSubject->id)
-                        ->where('evaluation_id', $evaluation->id)
-                        ->where('cycle', $data['cycle'])
-                        ->first();
+                if (!$enrollment) {
+                    $student = Student::find($data['student']);
 
-                    if ($data['note'] > $classeSubject->max_note) {
-                        return response()->json([
-                            'errors' => [
-                                __('messages.note_greater_than_max', [
-                                    'note' => $data['note'],
-                                    'max'  => $classeSubject->max_note
-                                ])
-                            ]
-                        ], 422);
-                    }
-
-                    if ($note) {
-                        $note->value = $data['note'];
-                        $note->save();
-                    }
-                    else {
-                        $note                    = new Note;
-                        $note->enrollment_id     = $enrollment->id;
-                        $note->classe_subject_id = $classeSubject->id;
-                        $note->evaluation_id     = $evaluation->id;
-                        $note->cycle             = $data['cycle'];
-                        $note->value             = $data['note'];
-                        $note->save();
-                    }
-
-                    $result[] = $note;
-                }
-                else {
                     return response()->json([
                         'errors' => [
-                            __(
-                                'classe_does_not_have_this_subject',
-                                ['classe' => $classe->name, 'subject' => $subject->name]
-                            )
+                            __('messages.student_not_enrolled_in_this_class', [
+                                'firstname'  => $student->firstname,
+                                'lastname'   => $student->lastname,
+                                'class_name' => $classe->name
+                            ])
                         ]
                     ], 422);
                 }
+
+                $note = Note::where('enrollment_id', $enrollment->id)
+                    ->where('classe_subject_id', $classeSubject->id)
+                    ->where('evaluation_id', $evaluation->id)
+                    ->where('cycle', $data['cycle'])
+                    ->first();
+
+                if ($data['note'] > $classeSubject->max_note) {
+                    return response()->json([
+                        'errors' => [
+                            __('messages.note_greater_than_max', [
+                                'note' => $data['note'],
+                                'max'  => $classeSubject->max_note
+                            ])
+                        ]
+                    ], 422);
+                }
+
+                if ($note) {
+                    $note->value = $data['note'];
+                    $note->save();
+                }
+                else {
+                    $note                    = new Note;
+                    $note->enrollment_id     = $enrollment->id;
+                    $note->classe_subject_id = $classeSubject->id;
+                    $note->evaluation_id     = $evaluation->id;
+                    $note->cycle             = $data['cycle'];
+                    $note->value             = $data['note'];
+                    $note->save();
+                }
+
+                $result[] = $note;
             }
             else {
                 return response()->json([
--- lang/fr/messages.php
+++ lang/fr/messages.php
@@ -1,12 +1,13 @@
 <?php
 
 return [
-    'student_not_found'                 => 'Étudiant non trouvé',
-    'at_least_one_student_not_found'    => 'Au moins un élève inexistant',
-    'the_data_has_been_saved'           => 'Les données ont bien été enregistrées',
-    'subject_name_already_exists'       => 'Le nom de discipline :name existe déjà',
-    'choosen_cycle_is_invalid'          => 'Le cycle choisi est supérieur au nombre de cycles',
-    'note_greater_than_max'             => 'Le note :note est plus grande que :max',
-    'classe_does_not_have_this_subject' => 'La classe :classe ne possède pas le sujet :subject',
-    'input_format_is_invalid'           => 'Le format des données est invalide'
+    'student_not_found'                  => 'Étudiant non trouvé',
+    'student_not_enrolled_in_this_class' => "L'étudiant :firstname :lastname n'est pas inscrit dans la classe :class_name",
+    'at_least_one_student_not_found'     => 'Au moins un élève inexistant',
+    'the_data_has_been_saved'            => 'Les données ont bien été enregistrées',
+    'subject_name_already_exists'        => 'Le nom de discipline :name existe déjà',
+    'choosen_cycle_is_invalid'           => 'Le cycle choisi est supérieur au nombre de cycles',
+    'note_greater_than_max'              => 'Le note :note est plus grande que :max',
+    'classe_does_not_have_this_subject'  => 'La classe :classe ne possède pas le sujet :subject',
+    'input_format_is_invalid'            => 'Le format des données est invalide'
 ];
