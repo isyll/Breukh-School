336707d6deb40ae1ca411fc1592802a22b14781a
Ibrahima Sylla <syllaisa6@gmail.com>
2023-07-08 21:53:55 +0000

    update cycle validation message

--- lang/fr/messages.php
+++ lang/fr/messages.php
@@ -6,7 +6,7 @@ return [
     'at_least_one_student_not_found'     => 'Au moins un élève inexistant',
     'the_data_has_been_saved'            => 'Les données ont bien été enregistrées',
     'subject_name_already_exists'        => 'Le nom de discipline :name existe déjà',
-    'choosen_cycle_is_invalid'           => 'Le cycle choisi est supérieur au nombre de cycles',
+    'choosen_cycle_is_invalid'           => 'Le cycle choisi est invalide',
     'note_greater_than_max'              => 'Le note :note est plus grande que :max',
     'classe_does_not_have_this_subject'  => 'La classe :classe ne possède pas le sujet :subject',
     'input_format_is_invalid'            => 'Le format des données est invalide'
